26e60a8815599aa483f2e3f7411901c8f71ed0b5
Onore849 <rion3376@gmail.com>
2020-08-28 22:21:25 +0900

    first commit

--- json Parse/ContentView.swift
+++ json Parse/ContentView.swift
@@ -7,10 +7,24 @@
 //
 
 import SwiftUI
+import  SDWebImageSwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var getData = Datas()
+
     var body: some View {
-        Text("Hello, World!")
+        
+        NavigationView {
+            
+            List(getData.jsonData) { i in
+                
+                ListRow(url: i.avatar_url, name: i.login)
+                
+            }.navigationBarTitle("Json Parsing")
+            
+        }
+        
     }
 }
 
@@ -19,3 +33,74 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+class Datas: ObservableObject {
+    
+    @Published var jsonData = [dataType]()
+    
+    init() {
+        
+        let session = URLSession(configuration: .default)
+        
+        session.dataTask(with: URL(string: "https://api.github.com/users/hadley/orgs")!) { (data, _, _) in
+            
+            
+            do {
+                
+                let fetch = try JSONDecoder().decode([dataType].self, from: data!)
+                
+                DispatchQueue.main.async {
+                    
+                    self.jsonData = fetch
+                    
+                }
+                
+            }
+            catch {
+                
+                print(error.localizedDescription)
+                
+            }
+            
+        }.resume()
+        
+        
+    }
+}
+
+struct dataType: Identifiable, Decodable {
+    
+    var id: Int
+    var login: String
+    var node_id: String
+    var avatar_url: String
+    
+    // I'm going to use only four attributes from json data
+}
+
+
+struct ListRow: View {
+    
+    var url: String
+    var name: String
+    
+    var body: some View {
+        
+        HStack {
+            
+            AnimatedImage(url: URL(string: url)).resizable().frame(width: 60, height: 60).clipShape(Circle()).shadow(radius: 20)
+            
+            Text(name).fontWeight(.heavy).padding(.leading, 10)
+            
+        }
+        
+        
+        
+    }
+    
+}
+
+
+// I'm going to add sdweb image package bcz I'm going to download image from json data.....
+
+
